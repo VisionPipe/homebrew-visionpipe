f09d8f359cbfb632a374097202c5fc1929f48310
DROdio <drodio@gmail.com>
2026-05-03 09:38:05 -0700

    Bump visionpipe to v0.3.8

--- Casks/visionpipe.rb
+++ Casks/visionpipe.rb
@@ -1,6 +1,6 @@
 cask "visionpipe" do
-  version "0.3.7"
-  sha256 "a40565109cbc168586e8edffced2b0f4e6ebd603bb78a80d440bbc3775b795aa"
+  version "0.3.8"
+  sha256 "125bf45b14d1f93ff6b1b0db002f6ed66a129c82f1cf19e06caf79ddc697e822"
 
   url "https://github.com/VisionPipe/visionpipe/releases/download/v#{version}/VisionPipe_#{version}_aarch64.dmg"
   name "VisionPipe"
