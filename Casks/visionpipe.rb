d60c1d694c9133f08e4600edf103a82af8f14033
DROdio <drodio@iMac.attlocal.net>
2026-05-06 13:21:54 -0700

    Bump visionpipe to v0.9.1

--- Casks/visionpipe.rb
+++ Casks/visionpipe.rb
@@ -1,6 +1,6 @@
 cask "visionpipe" do
-  version "0.9.0"
-  sha256 "b5384b9d7030bf0486d59880a6f388f532741a18b3ccd56f22a25ccbed79608d"
+  version "0.9.1"
+  sha256 "9126ab1f78f5fab44db2568e1a0352783f9c9c6b3ed76e37a63af79fe48c1529"
 
   url "https://github.com/VisionPipe/visionpipe/releases/download/v#{version}/VisionPipe_#{version}_aarch64.dmg"
   name "VisionPipe"
